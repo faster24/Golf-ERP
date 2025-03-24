36a8b9aabfa590d7029b9d018f4085ea763763f9
Nyi Ye Naing <45783468+faster24@users.noreply.github.com>
2025-03-24 16:08:43 +0700

    fix: access admin acc to panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,6 +51,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, 'https://cismo-main-bbgu3m.laravel.cloud');
+        $adminEmails = ['admin@admin.com', 'superadmin@example.com'];
+        return in_array($this->email, $adminEmails);
     }
 }
